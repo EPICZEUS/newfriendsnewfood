f8a626f13d01b6740ad81425acc312170400877b
EPICZEUS <s77broz@gmail.com>
2018-12-03 14:00:17 -0500

    Add functionality to User and Message controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,54 @@
 class UsersController < ApplicationController
+	before_each :find_user, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(user_params)
+
+		if @user.valid?
+			redirect_to @user
+		else
+			flash[:errors] = @user.errors.full_users
+			redirect_to new_user_path
+		end
+	end
+
+	def update
+		@user.update(user_params)
+
+		if @user.valid?
+			redirect_to @user
+		else
+			flash[:errors] = @user.errors.full_users
+			redirect_to edit_user_path
+		end
+	end
+	
+	def destroy
+		@user.destroy
+		redirect_to users_path
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	private
+
+	def find_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:name, :age, :username, :img_url, :bio)
+	end
 end
