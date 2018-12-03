8083089d4a41f1f67bcf21912efa40f6f0421475
jbmenashi <jbmenashi@gmail.com>
2018-12-03 14:00:12 -0500

    add CRUD for reservations and restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,55 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show, :edit, :create, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    #find_restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      flash[:errors] = @restaurant.errors.full_messages
+      redirect_to new_restaurant_path
+    end
+  end
+
+  def edit
+    #find_restaurant
+  end
+
+  def update
+    #find_restaurant
+    if @restaurant.update
+      redirect_to restaurant_path(@restaurant)
+    else
+      flash[:errors] = @restaurant.errors.full_messages
+      redirect_to edit_restaurant_path
+    end
+  end
+
+  def destroy
+    #find_restaurant
+    restaurant.destroy
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :location, :cuisine, :rating)
+  end
+
 end
