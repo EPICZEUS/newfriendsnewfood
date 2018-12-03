add01b10ee9175b20a6aa1363c78f84cd9d970fc
EPICZEUS <s77broz@gmail.com>
2018-12-03 14:54:32 -0500

    Add view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :reservations
   resources :restaurants
-  resources :messages
-  resources :groups
+  resources :groups do
+  	resources :messages, only: [:index, :create], controller: 'groups/messages'
+  end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
